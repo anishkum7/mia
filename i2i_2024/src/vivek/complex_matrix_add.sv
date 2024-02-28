942b3e05354e1859637ba678a1993b7a40159ce7
vn218 <72967949+vn218@users.noreply.github.com>
2024-02-28 12:45:25 -0500

    matrix add

---
+++ i2i_2024/src/vivek/complex_matrix_add.sv
@@ -0,0 +1,71 @@
+module complex_matrix_add
+#(
+     localparam SIZE = 16
+ )
+(
+  input logic                               clk_i,
+  input logic                               rst_ni,
+  // Input signals
+  input logic [SIZE*4-1:0][63:0]            operands_i, // {b2,a2,b1,a1}
+//   input fpnew_pkg::roundmode_e              rnd_mode_i,
+//   input fpnew_pkg::operation_e              op_i,
+//   input logic                               op_mod_i,
+  // Input Handshake
+  input  logic                              in_valid_i,
+  output logic                              in_ready_o,
+  input  logic                              flush_i,
+  input  logic                              sub,
+  // Output signals
+  output logic [SIZE*2-1:0][63:0]           result_o,
+  //output fpnew_pkg::status_t                status_o,
+  // Output handshake
+  output logic                              out_valid_o,
+  input  logic                              out_ready_i,
+  // Indication of valid data in flight
+  output logic                              busy_o
+
+);
+
+
+//logic                              out_ready_i,
+  // Indication of valid data in flight
+//logic                              busy_o
+logic [SIZE-1:0] complex_add_in_ready_o;
+logic [SIZE-1:0] complex_add_out_valid_o;
+logic [SIZE-1:0] complex_add_busy_o;
+
+assign out_valid_o = &complex_add_out_valid_o;
+assign in_ready_o = &complex_add_in_ready_o;
+assign busy_o = &complex_add_busy_o;
+
+genvar i;
+
+for (i = 0; i < SIZE; i=i+1) begin : add_loop
+complex_add
+complex_add
+(
+.clk_i(clk_i),
+.rst_ni(rst_i),
+  // Input signals
+.operands_i({operands_i[4*i+3],operands_i[4*i+2],operands_i[4*i+1],operands_i[4*i]}), // {b2,a2,b1,a1}
+  // Input Handshake
+.in_valid_i(in_valid_i),
+.in_ready_o(complex_add_in_ready_o[i]),
+.flush_i(flush_i),
+.sub(sub),
+  // Output signals
+.result_o(result_o[i]),
+//.status_o(status_o),
+  // Output handshake
+.out_valid_o(complex_add_out_valid_o[i]),
+.out_ready_i(out_ready_i),
+  // Indication of valid data in flight
+.busy_o(complex_add_busy_o[i])
+);
+
+end
+
+
+
+
+endmodule
---
+++ i2i_2024/src/vivek/tb_matrix.sv
@@ -0,0 +1,84 @@
+module tb;
+
+localparam NUM_OPERANDS=4,
+            WIDTH = 64,
+            SIZE = 16;
+
+logic                               clk_i;
+logic                               rst_ni;
+  // Input signals
+
+  // Input Handshake
+logic                              in_valid_i;
+logic                              in_ready_o;
+logic                              flush_i;
+logic [SIZE*NUM_OPERANDS-1:0][WIDTH-1:0] operands_i;
+  // Output signals
+logic [SIZE-1:0][WIDTH-1:0]             result_o;
+fpnew_pkg::status_t                status_o;
+logic                             tag_o;
+  // Output handshake
+logic                              out_valid_o;
+logic                              out_ready_i;
+  // Indication of valid data in flight
+logic                              busy_o;
+
+logic                              sub;
+
+
+initial clk_i = 0;
+always #5 clk_i = ~clk_i;
+
+initial begin
+sub = 1;
+rst_ni = 0;
+in_valid_i = 0;
+out_ready_i = 0;
+
+for (int i=0; i<SIZE; i=i+1) begin
+  operands_i[i*NUM_OPERANDS] = 64'h401c000000000000;
+  operands_i[i*NUM_OPERANDS+1] = 64'h4000000000000000;
+  operands_i[i*NUM_OPERANDS+2] = 64'h3ff0000000000000;
+  operands_i[i*NUM_OPERANDS+3] = 64'h4000000000000000;
+end
+
+flush_i = 0;
+#35
+rst_ni = 1;
+in_valid_i = 1;
+out_ready_i = 1;
+wait(out_valid_o == 1'b1);
+@(negedge clk_i);
+for (int i=0; i<SIZE; i=i+1) begin
+  $display("Answer %d : %x  %x", i, result_o[i+1], result_o[i]);
+end
+#1000
+$finish;
+end
+
+
+complex_matrix_add 
+// #(
+// .NUM_OPERANDS(3),
+// .WIDTH(64)
+// )
+DUT
+(
+.clk_i(clk_i),
+.rst_ni(rst_ni),
+.in_valid_i(in_valid_i),
+.in_ready_o(in_ready_o),
+.flush_i(flush_i),
+.operands_i(operands_i),
+.result_o(result_o),
+.status_o(status_o),
+//.tag_o(tag_o),
+.sub(sub),
+.out_valid_o(out_valid_o),
+.out_ready_i(out_ready_i),
+.busy_o(busy_o)
+
+);
+
+
+endmodule
