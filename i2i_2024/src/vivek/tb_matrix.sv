a61b466d6dfc866ec176c7869b5317ca5cad6586
vn218 <72967949+vn218@users.noreply.github.com>
2024-03-19 00:03:12 -0400

    .

--- i2i_2024/src/vivek/tb_matrix.sv
+++ i2i_2024/src/vivek/tb_matrix.sv
@@ -2,7 +2,7 @@ module tb_matrix;
 
 localparam NUM_OPERANDS=4,
             WIDTH = 64,
-            SIZE = 8;
+            SIZE = 16;
 
 logic                               clk_i;
 logic                               rst_ni;
