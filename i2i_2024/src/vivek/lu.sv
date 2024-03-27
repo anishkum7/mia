8b65b91bd7b620d3d1567a6b251da58a3847d065
vn218 <72967949+vn218@users.noreply.github.com>
2024-03-27 05:03:51 -0400

    lu

--- i2i_2024/src/vivek/lu.sv
+++ i2i_2024/src/vivek/lu.sv
@@ -311,7 +311,7 @@ div
   .operands_i(div_operands_i), // {b2,a2,b1,a1}
   .in_valid_i(div_in_valid_i),
   .in_ready_o(div_in_ready_o),
-  .flush_i(flush_i),
+  .flush_i(flush_i | rst_ni | div_out_valid_o),
   .result_o(div_result_o),
   .status_o(div_status_o),
   .out_valid_o(div_out_valid_o),
