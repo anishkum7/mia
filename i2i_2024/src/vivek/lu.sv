c94a0b8922ab5115e4d9e96ea617b174a37cbfce
vn218 <72967949+vn218@users.noreply.github.com>
2024-03-27 05:13:36 -0400

    lu

--- i2i_2024/src/vivek/lu.sv
+++ i2i_2024/src/vivek/lu.sv
@@ -307,11 +307,11 @@ complex_div
 div
 (
   .clk_i(clk_i),
-  .rst_ni(rst_ni & ~div_out_valid_o),
+  .rst_ni(rst_ni),
   .operands_i(div_operands_i), // {b2,a2,b1,a1}
   .in_valid_i(div_in_valid_i),
   .in_ready_o(div_in_ready_o),
-  .flush_i(flush_i),
+  .flush_i(flush_i | div_out_valid_o),
   .result_o(div_result_o),
   .status_o(div_status_o),
   .out_valid_o(div_out_valid_o),
