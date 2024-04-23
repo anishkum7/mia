2fd47b39c579ab0b8b36a5abcf51837b8beed22c
vn218 <72967949+vn218@users.noreply.github.com>
2024-04-23 16:32:27 -0400

    32 inv

--- i2i_2024/src/vivek/tb32.sv
+++ i2i_2024/src/vivek/tb32.sv
@@ -198,6 +198,7 @@ lu_result_out_ready_i = 1;
 lu_mat_row_out_ready_i = 1;
 triang_inv_out_ready_i = 1;
 complex_matrix_mul_out_ready_i = 1;
+complex_add_out_ready_i = 1;
 
 rst_ni = 1;
 
@@ -367,8 +368,8 @@ end
 
 always @ (posedge clk_i) begin
   
-  if (lu_in_ready_o)
-    $display("lu_ready = %d", lu_in_ready_o);
+  if (lu_mat_row_valid_o)
+    $display("lu_ready = %d", lu_mat_row_valid_o);
 
   lu_mat_row_i <= Matrix[lu_mat_row_read_addr_o];
   lu_mat_row_read_addr_i <= lu_mat_row_read_addr_o;
