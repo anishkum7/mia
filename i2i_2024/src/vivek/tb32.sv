1e9696a815dedd0ed10ae67d09a253a3790dfbd6
vn218 <72967949+vn218@users.noreply.github.com>
2024-04-24 10:17:18 -0400

    .

--- i2i_2024/src/vivek/tb32.sv
+++ i2i_2024/src/vivek/tb32.sv
@@ -262,6 +262,7 @@ for (int i = 0; i < SIZE; i=i+1) begin
         complex_matrix_mul_operands_i[k*NUM_OPERANDS+2] = Matrix1[k][j*2*WIDTH +: WIDTH];
         complex_matrix_mul_operands_i[k*NUM_OPERANDS+3] = Matrix1[k][j*2*WIDTH+WIDTH +: WIDTH];;  
         end
+        $display("%0h",complex_matrix_mul_result_o);
         U1[i][j*2*WIDTH +: 2*WIDTH] = complex_matrix_mul_result_o;
     end
 end
